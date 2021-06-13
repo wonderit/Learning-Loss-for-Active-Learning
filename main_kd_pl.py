d4a467eb493116b496ce547daf19eecb4159ed72
wonderit <wonsuk@broadinstitute.org>
2021-06-13 12:09:32 +0900

    kd ua add

--- main_kd_pl.py
+++ main_kd_pl.py
@@ -474,10 +474,10 @@ if __name__ == '__main__':
                 labeled_indices = list(torch.tensor(subset)[arg][:PARAMS['k']].numpy())
                 labeled_set += labeled_indices
                 unlabeled_set = list(torch.tensor(subset)[arg][PARAMS['k']:].numpy()) + unlabeled_set[PARAMS['subset_size']:]
-                print('prev targets:', cifar10_train.targets[labeled_indices[0]])
+                print(f'prev targets:{labeled_indices[0]}', cifar10_train.targets[labeled_indices[0]])
 
-                for l_i in labeled_indices:
-                    cifar10_train.targets[l_i] = pseudo_label[l_i]
+                for l_i in range(len(labeled_indices)):
+                    cifar10_train.targets[labeled_indices[l_i]] = pseudo_label[l_i]
                 print(f'prev targets:{labeled_indices[0]}', cifar10_train.targets[labeled_indices[0]])
 
             # Create a new dataloader for the updated labeled dataset
