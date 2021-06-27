d62c1ae5f603081575085e02a2985529b4a3d4da
wonderit <wonsuk@broadinstitute.org>
2021-06-27 16:25:10 +0900

    margin0.01 reinit

--- main_cem.py
+++ main_cem.py
@@ -56,7 +56,7 @@ from sklearn.metrics import r2_score, mean_squared_error
 
 
 run = neptune.init(project='wonderit/maxwellfdfd-ll4al',
-                   tags='cem',
+                   tags=['margin0.01', 're-init'],
                    api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2ZmY3ZjczOC0wYWM2LTQzZGItOTNkZi02Y2Y3ZjkxMDZhZTgifQ==')
 
 
