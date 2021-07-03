98ab14cba3bd96dfd4ee8bc6e709e195e96fa2bc
wonderit <wonsuk@broadinstitute.org>
2021-07-03 16:01:02 +0900

    plot cem

--- main_cem.py
+++ main_cem.py
@@ -56,7 +56,7 @@ from sklearn.metrics import r2_score, mean_squared_error
 
 
 run = neptune.init(project='wonderit/maxwellfdfd-ll4al',
-                   tags=['margin0.5', 'sub20000', 'reinit'],
+                   tags=['margin0.1', 'sub20000', 'no-init'],
                    api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2ZmY3ZjczOC0wYWM2LTQzZGItOTNkZi02Y2Y3ZjkxMDZhZTgifQ==')
 
 
@@ -90,8 +90,8 @@ PARAMS = {
     'ua_activation': 'sigmoid', # linear, relu
     'beta': 1.0,
     'ua_type': 'prediction_loss',  # prediction_loss, cross_entropy
-    're-init-backbone': True,
-    're-init-module': True,
+    're-init-backbone': False,
+    're-init-module': False,
     'is_tbr': False,
     'tbr_lambda': 0.5,
     'is_random': False,
