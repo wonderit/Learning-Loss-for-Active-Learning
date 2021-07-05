4f93daed4b1e3752814c6357b2764c8c715df625
wonderit <wonsuk@broadinstitute.org>
2021-07-05 12:01:45 +0900

    tor-tbr-k500

--- main_cem_k500.py
+++ main_cem_k500.py
@@ -56,7 +56,7 @@ from sklearn.metrics import r2_score, mean_squared_error
 
 
 run = neptune.init(project='wonderit/maxwellfdfd-ll4al',
-                   tags=['margin0.1', 'sub20000', 're-init', 'k500', 'll'],
+                   tags=['margin0.1', 'sub20000', 're-init', 'k500', 'll', 'tor_tbr'],
                    api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2ZmY3ZjczOC0wYWM2LTQzZGItOTNkZi02Y2Y3ZjkxMDZhZTgifQ==')
 
 
@@ -86,13 +86,13 @@ PARAMS = {
     'is_random': False,
     're-init-backbone': True,
     're-init-module': True,
-    'is_kd': False,
-    'is_tbr': False,
+    'is_kd': True,
+    'is_tbr': True,
     'tbr_lambda': 0.9,
-    'is_tor': False,
+    'is_tor': True,
     'tor_lambda': 0.9,
     'tor_zscore': 2.0,
-    'server': 2,
+    'server': 7,
 }
 
 
